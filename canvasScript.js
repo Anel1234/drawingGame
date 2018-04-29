2aa0594a3ce594cbabbf365491d6bee94789d8c5
Anel Pilav <anel.pilav@gmail.com>
2018-04-29 23:07:20 +0100

    fb

--- canvasScript.js
+++ canvasScript.js
@@ -207,11 +207,11 @@ function facebookLogin(){
 };
 
 function facebookSend(){
-    // FB.ui({
-    //     method: 'send',
-    //     name: 'This is a test - DO NOT BE ALARMED',
-    //     link: 'https://anel1234.github.io/drawingGame/drawnTogether'
-    // })
+    FB.ui({
+        method: 'send',
+        name: 'This is a test - DO NOT BE ALARMED',
+        link: 'https://anel1234.github.io/drawingGame/drawnTogether'
+    })
     FB.ui({
         method: 'apprequests',
         // to: "",
